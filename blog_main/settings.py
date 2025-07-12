4606de38aa7eab0dd4dbb4be271904ee049e0f62
Hridaya Shrestha <129663764+ridayasth4@users.noreply.github.com>
2025-07-12 22:48:41 +0545

    Update settings.py

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -6,6 +6,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Quick-start development settings - unsuitable for production
